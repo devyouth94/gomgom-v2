89e6a3bb25bc2779654e8583951e94919ce3ecae
devyouth94 <devyouth94@gmail.com>
2023-01-21 22:49:19 +0900

    Chore: 리액트 쿼리 기본 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { useCallback, useEffect, useState } from 'react';
 import { Provider } from 'react-redux';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ThemeProvider } from 'styled-components';
 
 import { store } from 'app/config/store';
@@ -7,6 +9,10 @@ import Router from 'router/Router';
 import useThemeStore from 'stores/useThemeStore';
 import GlobalStyles from 'styles/GlobalStyles';
 import { darkTheme, defaultTheme } from 'styles/theme';
+
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { refetchOnWindowFocus: false } },
+});
 
 const App = () => {
   const theme = useThemeStore((state) => state.theme);
@@ -25,12 +31,16 @@ const App = () => {
   }, [screenSize]);
 
   return (
-    <Provider store={store}>
-      <ThemeProvider theme={theme === 'dark' ? darkTheme : defaultTheme}>
-        <GlobalStyles />
-        <Router />
-      </ThemeProvider>
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <ThemeProvider theme={theme === 'dark' ? darkTheme : defaultTheme}>
+          <GlobalStyles />
+          <Router />
+        </ThemeProvider>
+      </Provider>
+
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 };
 
