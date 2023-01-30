e8f54e7d12d7677391d8abc133f0c72d9f191843
devyouth94 <devyouth94@gmail.com>
2023-01-30 17:15:37 +0900

    Refactor: 삭제된 게시글 get할 경우 메인 페이지로 navigate

--- src/domains/select/hooks/useGetDetail.ts
+++ src/domains/select/hooks/useGetDetail.ts
@@ -1,7 +1,9 @@
+import axios from 'axios';
 import { useQuery } from '@tanstack/react-query';
 
 import instance from 'app/instance';
 import type { DetailItemProps } from 'lib/constants/types';
+import { useNavigate } from 'react-router-dom';
 
 type Data = {
   msg: string;
@@ -15,7 +17,18 @@ const getDetail = async (selectKey: number) => {
 };
 
 const useGetDetail = (selectKey: number) => {
-  return useQuery(['detail', selectKey], () => getDetail(selectKey));
+  const navigate = useNavigate();
+
+  return useQuery(['detail', selectKey], () => getDetail(selectKey), {
+    onError: (error) => {
+      if (axios.isAxiosError(error)) {
+        const message = (error.response?.data as { errMsg?: string }).errMsg;
+        if (message?.includes('존재하지')) {
+          navigate('/select', { replace: true });
+        }
+      }
+    },
+  });
 };
 
 export default useGetDetail;
