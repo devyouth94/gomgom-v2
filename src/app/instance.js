403908467204adf5ac2cd7dc2779d7c6e584419c
devyouth94 <devyouth94@gmail.com>
2023-02-01 15:21:47 +0900

    Refactor: 인스턴스 설정 변경

---
+++ src/app/instance.js
@@ -0,0 +1,47 @@
+import axios from 'axios';
+import { userStorage } from 'lib/utils/storage';
+
+const instance = axios.create({
+  baseURL: process.env.REACT_APP_API,
+  headers: {
+    accessToken: `Bearer ${userStorage.getToken('access')}`,
+    refreshToken: `Bearer ${userStorage.getToken('refresh')}`,
+  },
+});
+
+instance.interceptors.request.use((config) => {
+  if (config.headers) {
+    config.headers.accessToken = `Bearer ${userStorage.getToken('access')}`;
+    config.headers.refreshToken = `Bearer ${userStorage.getToken('refresh')}`;
+  }
+
+  return config;
+});
+
+instance.interceptors.response.use(
+  (config) => {
+    if (config.status === 201 && config.data.msg.includes('재발급')) {
+      return axios({
+        ...config.config,
+        headers: {
+          accessToken: `Bearer ${config.data.accessToken}`,
+          refreshToken: `Bearer ${config.data.refreshToken}`,
+        },
+      }).then(() => {
+        localStorage.setItem('accessToken', config.data.accessToken);
+        localStorage.setItem('refreshToken', config.data.refreshToken);
+      });
+    } else {
+      return config;
+    }
+  },
+
+  (error) => {
+    if (error.response.status === 401 && error.response.data.errMsg.includes('만료')) {
+      localStorage.clear();
+    }
+    return Promise.reject(error);
+  },
+);
+
+export default instance;
