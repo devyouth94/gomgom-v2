7e124eb4d166696238cb408212fe739696b6cd42
devyouth94 <devyouth94@gmail.com>
2023-02-10 14:11:33 +0900

    Refactor: 댓글 시간 표시 함수 변경

--- src/lib/utils/timeCalculation.ts
+++ src/lib/utils/timeCalculation.ts
@@ -17,6 +17,26 @@ export const remainedTime = (value: string) => {
   if (months < 12) return `${Math.floor(months)}개월`;
 };
 
+export const commentTime = (value: string) => {
+  const time = Math.abs(Number(new Date(value)) - Number(new Date()));
+
+  const minutes = time / 60000;
+  if (minutes < 1) return `방금`;
+  if (minutes < 60) return `${Math.floor(minutes)}분`;
+
+  const hours = minutes / 60;
+  if (hours < 24) return `${Math.floor(hours)}시간`;
+
+  const days = hours / 24;
+  if (days < 7) return `${Math.floor(days)}일`;
+
+  const weeks = days / 7;
+  if (weeks < 5) return `${Math.floor(weeks)}주`;
+
+  const months = days / 30;
+  if (months < 12) return `${Math.floor(months)}개월`;
+};
+
 export const nowTime = (value: string) => {
   const date = new Date(value);
 
